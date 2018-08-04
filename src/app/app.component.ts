3557155651e76376eea11a0abf38268021f7248a
indianajim <jimrunkel@gmail.com>
2018-08-04 16:57:54 -0400

    initial commit - new Angular practice app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'todo-app';
+  title = "My Todo's";
+  todo = {
+    label: "Bring Milk",
+    done: false,
+    priority: 3
+  };
 }
